8be73005d340d18cac329ce2382f56655d88d7c5
mikhailbro <brodski@gmail.com>
2022-01-21 20:02:09 +0100

    renaming

--- app_interface/urls.py
+++ app_interface/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from app_interface import views as interface_views
 
 urlpatterns = [
-    path('', interface_views.my_interfaces, name='my_interfaces'),
+    path('my_interfaces', interface_views.my_interfaces, name='my_interfaces'),
     path('interface/<interface_id>', interface_views.interface_details, name='interface_details'),
     path('update_interface/<interface_id>', interface_views.update_interface, name='update_interface'),
     path('create_interface', interface_views.create_interface, name='create_interface'),
